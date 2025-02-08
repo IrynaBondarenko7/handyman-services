a7a5198f9535ab0f5363c61e82b8966a41beb48b
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-08 20:13:00 +0000

    updated reviews swiper buttons styles & updated hero section styles & changed gallerySwiper function

--- js/gallerySwiper.js
+++ js/gallerySwiper.js
@@ -4,14 +4,30 @@ export const gallerySwiper = () => {
   const swiper = new Swiper(".swiper", {
     speed: 3000,
     loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-
+    autoplay: false,
     scrollbar: {
       el: ".swiper-scrollbar",
       draggable: true,
     },
   });
+
+  let autoplayStarted = false;
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !autoplayStarted) {
+          swiper.autoplay.start();
+          autoplayStarted = true;
+          observer.disconnect();
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  const swiperContainer = document.querySelector(".swiper");
+  if (swiperContainer) {
+    observer.observe(swiperContainer);
+  }
 };
